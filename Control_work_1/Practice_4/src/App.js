57b520ce15b73fd79bf21dbd3123e28ad37fdf70
Zhuzhleva Ksenia <kseniazuzleva2395@gmail.com>
2026-02-19 17:46:20 +0300

    create basic structure for practice 4

---
+++ Control_work_1/Practice_4/src/App.js
@@ -0,0 +1,12 @@
+import UsersPage from "./pages/UsersPage/UsersPage";
+
+// главный компонент приложения
+function App() {
+    return (
+        <div className="App">
+            <UsersPage />
+        </div>
+    );
+}
+
+export default App;
--- Control_work_1/Practice_4/src/index.js
+++ Control_work_1/Practice_4/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// точка входа в приложение
 root.render(
     <React.StrictMode>
     <App/>
